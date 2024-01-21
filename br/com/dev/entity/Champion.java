7084ee13b8eb902c8e8b03acd08878fe1aca5702
fabiosimplis <fabio.simplis@gmail.com>
2024-01-21 18:49:59 -0300

    Add close

--- br/com/dev/entity/Champion.java
+++ br/com/dev/entity/Champion.java
@@ -1,2 +1,78 @@
+package br.com.dev.entity;
+
 public class Champion {
+
+    private String name;
+    private int life;
+    private int attack;
+    private int armor;
+
+    public Champion(String name, int life, int attack, int armor) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void takeDamage(Champion champion){
+
+        int attack = attackToArmor(champion.getAttack());
+
+        this.life = this.life - attack;
+
+        if(this.life < 0){
+            this.life = 0;
+        }
+    }
+
+    private int attackToArmor(int championAttack) {
+
+        int attack = championAttack - this.armor;
+
+        if (attack <= 0)
+            return 1;
+
+        return attack;
+    }
+
+    public String status(){
+
+        String morreu = this.life > 0 ? "" : " (morreu)";
+
+        return this.name + " : " + this.life + " de vida" + morreu;
+    }
+
+
 }
--- br/com/dev/teste/main.java
+++ br/com/dev/teste/main.java
@@ -1,4 +1,66 @@
 package br.com.dev.teste;
 
+import br.com.dev.entity.Champion;
+
+import java.util.Scanner;
+
 public class main {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("\nDigite os dados do primeiro campeão:");
+        Champion champion1 = createChampion(leitor);
+        System.out.println("\nDigite os dados do segundo campeão:");
+        Champion champion2 = createChampion(leitor);
+
+        System.out.print("\nQuantidades de Turno: ");
+        int rodadas = leitor.nextInt();
+        int i = 1;
+        while(i <= rodadas && (champion1.getLife() > 0 && champion2.getLife() > 0)){
+
+            System.out.printf("\nResultado do turno %d:\n", i );
+            champion1.takeDamage(champion2);
+            champion2.takeDamage(champion1);
+
+            System.out.println(champion1.status());
+            System.out.println(champion2.status());
+
+            i++;
+
+        }
+        leitor.close();
+        System.out.println("FIM DO COMBATE");
+    }
+
+    public static int validaEntrada(String msg, Scanner leitor){
+        System.out.print(msg);
+        int entrada = leitor.nextInt();
+
+        while (entrada <= 0){
+            System.out.println("Valor invalido! Favor digitar um valor positivo: " + entrada);
+            entrada = leitor.nextInt();
+        }
+
+        return entrada;
+    }
+
+    public static Champion createChampion(Scanner leitor){
+
+        String nome = " ";
+        int vida = 0;
+        int ataque = 0;
+        int armadura = 0;
+
+
+        System.out.print("Nome: ");
+        nome = leitor.next();
+        vida = validaEntrada("Vida Inicial: ", leitor);
+        ataque = validaEntrada("Ataque: ", leitor);
+        armadura = validaEntrada("Armadura: ", leitor);
+
+        return new Champion(nome, vida, ataque, armadura);
+    }
+
+
 }
